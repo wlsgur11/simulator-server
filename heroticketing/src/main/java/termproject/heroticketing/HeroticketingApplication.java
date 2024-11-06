eb0d8ad9921c3651ff213e0ed893bb07d2edcdef
student-p <paulmoon00@naver.com>
2024-11-06 20:00:34 +0900

    init: 프로젝트 초기 설정

---
+++ heroticketing/src/main/java/termproject/heroticketing/HeroticketingApplication.java
@@ -0,0 +1,13 @@
+package termproject.heroticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroticketingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeroticketingApplication.class, args);
+    }
+
+}
---
+++ heroticketing/src/test/java/termproject/heroticketing/HeroticketingApplicationTests.java
@@ -0,0 +1,13 @@
+package termproject.heroticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroticketingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
